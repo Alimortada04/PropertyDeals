d75baa9a740211f9fa512674eedb99f0d82d7909
alimortada04 <40895468-alimortada04@users.noreply.replit.com>
2025-06-01 19:20:57 +0000

    Restored to '642579c7c04c4527f2aa0907b8fe55ab7ee0dc55'
    Replit-Restored-To: 642579c7c04c4527f2aa0907b8fe55ab7ee0dc55

--- client/src/components/profile/mobile-settings-section.tsx
+++ client/src/components/profile/mobile-settings-section.tsx
@@ -33,7 +33,7 @@ const MobileSettingsSection = ({ section, title, children }: MobileSettingsSecti
       </div>
 
       {/* Section Content */}
-      <div className="p-4 pb-20">
+      <div className="p-4">
         {children}
       </div>
     </div>
